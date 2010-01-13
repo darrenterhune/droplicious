6caf3ef8ff361413c30270c6de308ed7f94b0c91
Darren Terhune <darren@headfirstproductions.ca>
2010-01-13 11:51:03 -0800

    refactored javascript, removed redundant code

--- javascripts/droplicious.js
+++ javascripts/droplicious.js
@@ -1,91 +1,102 @@
 /*
-droplicious v.1.0
-Copyright @2009 http://headfirstproductions.ca
-Author: Darren Terhune
-Contributors: Jan Sovak http://canada-jack.com, Mason Meyer http://www.masonmeyer.com/
-Created May 21, 2009
+droplicious v.1.0 Created May 21, 2009
+Copyright @2009 http://headfirstproductions.ca Author: Darren Terhune
+Contributors: Jan Sovak http://canada-jack.com,  Mason Meyer http://www.masonmeyer.com
+This software is licensed under the Creative Commons Attribution 2.5 Canada License 
+<http://creativecommons.org/licenses/by/2.5/ca//>
 */
 
 var dropliciousShowingUpDuration = 0.3;
 var dropliciousHidingDuration = 0.1;
 var dropliciousHideDelay = 0;
 
-function dropliciousShowingUpEffect(dEl){
-	 if(!Element.visible(dEl))
-	new Effect.BlindDown( $(dEl),  { duration: dropliciousShowingUpDuration, queue: {position: 'end', scope: dEl.id, limit:2} } );
+function dropliciousShowingUpEffect(element){
+	if(!element.visible()){
+		new Effect.BlindDown(element, {
+			duration: dropliciousShowingUpDuration,
+			queue: {
+				position: 'end',
+				scope: element.identify(),
+				limit:2
+			}
+		});
+	}
 }
-function dropliciousHidingEffect(dEl){
-	new Effect.BlindUp( $(dEl),  { duration: dropliciousHidingDuration, queue: {position: 'end', scope: dEl.id, limit:2 } });
+
+function dropliciousHidingEffect(element){
+	new Effect.BlindUp(element, {
+		duration: dropliciousHidingDuration,
+		queue: {
+			position: 'end',
+			scope: element.identify(),
+			limit: 2
+		}
+	 });
 }
-function setDelayedHide(id){
-	$(id).addClassName('waitingtohide')
-	if(!$(id).hasClassName('hidding')){
-		if (!$(id).hasClassName('hiddingtimerset')){	
-			$(id).addClassName('hiddingtimerset');
-			setTimeout("delayedHide('" + id + "')", dropliciousHideDelay * 1000);
+
+function setDelayedHide(element){
+	element.addClassName('waitingtohide')
+	if(!element.hasClassName('hidding')){
+		if (!element.hasClassName('hiddingtimerset')){	
+			element.addClassName('hiddingtimerset');
+			setTimeout(function(){ delayedHide(element); }, dropliciousHideDelay * 1000);
 		}
 	}
 }
-function delayedHide(id){
-	var dropElement = $(id);
+function delayedHide(dropElement){
 	dropElement.removeClassName('hiddingtimerset');
-	if ($(dropElement).hasClassName('waitingtohide')){
+	if (dropElement.hasClassName('waitingtohide')){
 		dropliciousHidingEffect(dropElement);
-		$(dropElement).addClassName('hidding');
-		setTimeout("finishedHiding('" + id + "')",dropliciousHidingDuration * 1000);
+		dropElement.addClassName('hidding');
+		setTimeout(
+			function(){
+				dropElement.removeClassName('waitingtohide');
+				dropElement.removeClassName('hidding');
+				dropElement.removeClassName('active');
+			}, dropliciousHidingDuration * 1000);
 	}
 }
-function finishedHiding(id){
-	var dropElement = $(id);
-	dropElement.removeClassName('waitingtohide');
-	dropElement.removeClassName('hidding');
-	dropElement.removeClassName('active');
-}
+
 function linkMouseOut(id){
-	var currentElement = Event.element(id).id;
-	var currentElement = $(currentElement);
-	var dropElement = currentElement.next();		
-	if ($(dropElement).hasClassName('active')){
-		setDelayedHide($(dropElement).id);
+	var dropElement = id.element().next();		
+	if (dropElement && dropElement.hasClassName('active')){
+		setDelayedHide(dropElement);
 	}
 }
 function linkMouseOver(id){
-	var currentElement = Event.element(id).id;
-	var currentElement = $(currentElement);
-	var dropElement = currentElement.next();	
-	if (!$(dropElement).hasClassName('hidding')){
-		dropElement.removeClassName('waitingtohide');
-	}
-	if (!$(dropElement).hasClassName('active')){
-		dropElement.addClassName('active');
-		dropliciousShowingUpEffect(dropElement);
-	}
-}
-function submenuMouseOut(id){
-	var currentElement = Event.findElement(id,'ul');
-	var currentElement = $(currentElement);
-	var dropElement = currentElement;	
-	if ($(dropElement).hasClassName('active')){
-		setDelayedHide($(dropElement).id);
+	var dropElement = id.element().next();
+	if(dropElement){
+		if (!dropElement.hasClassName('hidding')){
+			dropElement.removeClassName('waitingtohide');
+		}
+		if (!dropElement.hasClassName('active')){
+			dropElement.addClassName('active');
+			dropliciousShowingUpEffect(dropElement);
+		}
 	}
 }
-function submenuMouseOver(id){
-	var currentElement = Event.findElement(id,'ul');
-	var currentElement = $(currentElement);
-	var dropElement = currentElement;
-	if (!$(dropElement).hasClassName('hidding')){
+function submenuMouseOut(event){
+	var dropElement = event.findElement("ul");	
+	if (dropElement && dropElement.hasClassName('active')){
+		setDelayedHide(dropElement);
+	}
+}
+
+function submenuMouseOver(event){
+	var dropElement = event.findElement("ul");	
+	if (dropElement && !dropElement.hasClassName('hidding')){
 		dropElement.removeClassName('waitingtohide');
 	}
 }
+
 document.observe('dom:loaded', function() {
-	var dropDowns = $$('a.drops');	
-	dropDowns.each(function(name) {
+	$$('a.drops').each(function(name) {
 		name.observe('mousemove', linkMouseOver.bindAsEventListener(this));
-		name.observe('mouseout', linkMouseOut.bindAsEventListener(this));
-	})
-	var dropDowns = $$('ul.scriptaculously');
-	dropDowns.each(function(name){
+		name.observe('mouseout',  linkMouseOut.bindAsEventListener(this));
+	});
+
+	$$('ul.scriptaculously').each(function(name){
 		name.observe('mousemove', submenuMouseOver.bindAsEventListener(this));
-		name.observe('mouseout', submenuMouseOut.bindAsEventListener(this));
-	})
+		name.observe('mouseout',  submenuMouseOut.bindAsEventListener(this));
+	});
 })
